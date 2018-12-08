51ada1681cf6f958dacac9e1b22bbb7cfac8c5b4
Hao Li <haoli2@andrew.cmu.edu>
2018-12-08 15:19:06 -0500

    new template, adding more content

---
+++ sankey/sankey-spring16.js
@@ -0,0 +1,42 @@
+
+
+sankeyDraw(
+  {"nodes": 
+  	[
+       {"name":'Introduction'},
+       {"name":'Iteration'},
+       {"name":'Recursion'},
+       {"name": 'Data Representation'},
+       {"name": 'Cellular Automata'},
+       {"name":'Encryption'},
+       {"name": 'Computability'},
+       {"name":'Data Structures'},
+       {"name":'Time-efficiency'}
+    ],
+
+    "links": 
+    [
+		{"source":0,"target":6,"value":106},
+		{"source":0,"target":1,"value":44},
+		{"source":0,"target":2,"value":2},
+		{"source":2,"target":6,"value":4},
+		{"source":1,"target":6,"value":15},
+		{"source":1,"target":2,"value":20},
+		{"source":2,"target":3,"value":13},
+		{"source":3,"target":4,"value":8},
+		{"source":4,"target":5,"value":8},
+		{"source":5,"target":6,"value":8},
+		{"source":6,"target":7,"value":10},
+		{"source":3,"target":6,"value":3},
+		{"source":1,"target":0,"value":1},
+		{"source":2,"target":7,"value":1},
+		{"source":7,"target":6,"value":2},
+		{"source":3,"target":5,"value":1},
+		{"source":0,"target":7,"value":1},
+		{"source":1,"target":3,"value":2},
+		{"source":2,"target":1,"value":1},
+		{"source":6,"target":1,"value":1}
+    ],
+    'sinksRight': false
+  }
+);
